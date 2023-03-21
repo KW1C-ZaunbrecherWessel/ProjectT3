69d5bb4ac5c0eac7cc8cfb8ea22ff863892ab1cf
Administrator <wessel.zaunbrecher@edu-kw1c.nl>
2023-03-21 12:14:05 +0100

    update

---
+++ wip.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * auteur:         wessel zaunbrecher
+ * aanmaakdatum:   21-3-2023
+ * omschrijving:   ProjectT3
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/stylesheet.css">
+    <title>
+        Work In Progress
+    </title>
+</head>
+<?php
+include "includes/header.php";
+?>
+<body>
+<main>
+<article>
+    <figure>
+        <img src="images/under-construction.png">
+    </figure>
+</article>
+</main>
+</body>
+</html>
