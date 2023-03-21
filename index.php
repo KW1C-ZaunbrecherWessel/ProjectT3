5311a6c9019e2f5153975b90408aa3599f837511
Administrator <wessel.zaunbrecher@edu-kw1c.nl>
2023-03-21 10:08:16 +0100

    update

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php
 /*
  * auteur:         wessel zaunbrecher
- * aanmaakdatum:   13-02-2023
- * omschrijving:   praktijkopdracht t3
+ * aanmaakdatum:   21-3-2023
+ * omschrijving:   ProjectT3
  */
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/stylesheet.css">
+    <title>
+        Mc Donald's Burgerquiz
+    </title>
+</head>
+<body>
+    <main>
+        <article>
+        </article>
+    </main>
+</body>
+</html>
--- survey.php
+++ survey.php
@@ -1,2 +1,19 @@
 <?php
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/stylesheet.css">
+    <title>
+        Mc Donald's Burgerquiz
+    </title>
+</head>
+<body>
+<main>
+    <article>
+    </article>
+</main>
+</body>
+</html>
