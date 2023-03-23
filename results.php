20e7601ac4552d8f0e3eb5931eda82f471f194db
StefTax <stef.tax@edu-kw1c.nl>
2023-03-23 12:38:21 +0100

    results.php

--- results.php
+++ results.php
@@ -1,19 +1,79 @@
 <?php
+/*
+Auteur: Stef Tax
+Datum: 21-03-23
+Omschrijving: Dit is de resultaten pagina van de form
+*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/stylesheet.css">
-    <title>
-        Mc Donald's Burgerquiz
-    </title>
-</head>
-<body>
-<main>
-    <article>
-    </article>
-</main>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./styles/stylesheet.css">
+        <title>
+            Mc Donald's Burgerquiz
+        </title>
+    </head>
+    <?php
+    //include header and nav
+        require_once "includes/header.php"
+    ?>
+
+    <body>
+        <main>
+            <article>
+                <?php
+//                question 1
+                    $vraag1 = $_POST['vraag1'];
+//                question 2
+                    $vraag2 = $_POST['vraag2'];
+//                question 3
+                    $vraag3 = $_POST['vraag3'];
+//                question 4
+                    $vraag4 = $_POST['vraag4'];
+//                question 5
+                    $vraag5 = $_POST['vraag5'];
+//                question 6
+                    $vraag6 = $_POST['vraag6'];
+//                question 7
+                    $vraag7 = $_POST['vraag7'];
+//                question 8
+                    $vraag8 = $_POST['vraag8'];
+//                question 9
+                    $vraag9 = $_POST['vraag9'];
+//                question 10
+                    $vraag10 = $_POST['vraag10'];
+//                question 11
+                    $vraag11= $_POST['vraag11'];
+//                question 12
+                    $vraag12 = $_POST['vraag12'];
+//                    Create if else statements to determine which burger fits best by points
+                $allevragen = $vraag1 + $vraag2 + $vraag3 + $vraag4 + $vraag5 + $vraag6 + $vraag7 + $vraag8 + $vraag9 + $vraag10 + $vraag11 + $vraag12;
+                if ($allevragen <= 100)
+                {
+                    echo "De Mc Plant past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                }
+                else if ($allevragen <= 200)
+                {
+                    echo "De Mc Chicken past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                }
+                else if ($allevragen <= 300)
+                {
+                    echo "De Quarter Pounder past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                }
+                else
+                {
+                    echo "De Big Mac past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                }
+                ?>
+
+            </article>
+        </main>
+        <!--footer-->
+        <?php
+        require_once "includes/footer.php";
+        ?>
 </body>
+
 </html>
