32d1f9ed311ed3215b5bf1dc6852f3e01e15465a
StefTax <stef.tax@edu-kw1c.nl>
2023-03-23 12:37:29 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,17 @@
-<?php
-?>
+<footer>
+    <a href="../wip.php">Disclaimer</a>
+    <ul id="lijstje1">
+        <li>
+            <a href="../wip.php">Privacy</a>
+        </li>
+        <li>
+            <a href="../wip.php">Coockies</a>
+        </li>
+    </ul>
+    <ul id="lijstje2">
+        <li>
+<!--            add an image of the logo here-->
+            <img src=""> &copy; Copyright &copy; 2022 McDonald's Nederland.
+        </li>
+    </ul>
+</footer>
