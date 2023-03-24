3dd4c911aa1cb7ffacbe5d5c1ddb5ef9987ac965
StefTax <stef.tax@edu-kw1c.nl>
2023-03-24 09:54:05 +0100

    survey

--- survey.php
+++ survey.php
@@ -2,18 +2,18 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/stylesheet.css">
-    <title>
-        Mc Donald's Burgerquiz
-    </title>
-</head>
-<body>
-<main>
-    <article>
-    </article>
-</main>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./styles/stylesheet.css">
+        <title>
+            Mc Donald's Burgerquiz
+        </title>
+    </head>
+    <body>
+    <main>
+        <article>
+        </article>
+    </main>
+    </body>
 </html>
