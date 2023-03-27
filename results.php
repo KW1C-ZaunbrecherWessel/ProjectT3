9cb125a46ed85c42fce6b4284f055075999f66a7
StefTax <stef.tax@edu-kw1c.nl>
2023-03-27 12:37:13 +0200

    results

--- results.php
+++ results.php
@@ -48,23 +48,34 @@ Omschrijving: Dit is de resultaten pagina van de form
                     $vraag11= $_POST['vraag11'];
 //                question 12
                     $vraag12 = $_POST['vraag12'];
+//                    questions for name, email and age
+//                name
+                    $vraag13 = $_POST['vraag13'];
+//                email
+                    $vraag14 = $_POST['vraag14'];
+//                age
+                    $vraag15 = $_POST['vraag15'];
 //                    Create if else statements to determine which burger fits best by points
                 $allevragen = $vraag1 + $vraag2 + $vraag3 + $vraag4 + $vraag5 + $vraag6 + $vraag7 + $vraag8 + $vraag9 + $vraag10 + $vraag11 + $vraag12;
+
+//                display name time and date
+                echo "Bedankt voor het invullen van de vragenlijst " . $vraag14 . "!";
+                require_once "includes/tijd.php";
                 if ($allevragen <= 100)
                 {
-                    echo "De Mc Plant past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                    echo "De Mc Plant past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten" . "<br>" . "<a href='https://www.mcdonalds.com/nl/nl-nl/product/McPlant.html#accordion-7b89f50203-item-9dc886373b'> klik hier voor meer informatie.</a>" . "<br>" . "<img src='images/McPlant.jpg' alt='Hier hoort een Mc Plant te staan'>";
                 }
                 else if ($allevragen <= 200)
                 {
-                    echo "De Big Mac past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                    echo "De Big Mac past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten" . "<br>" . "<a href='https://www.mcdonalds.com/nl/nl-nl/product/big-mac.html#accordion-7b89f50203-item-9dc886373b'> klik hier voor meer informatie</a>" . "<br>" . "<img src='images/BigMac.jpg' alt='Hier hoort een Big Mac te staan'>";
                 }
                 else if ($allevragen <= 300)
                 {
-                    echo "De Quarter Pounder past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                    echo "De Quarter Pounder past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten" . "<br>" . "<a href='https://www.mcdonalds.com/nl/nl-nl/product/quarter-pounder.html#accordion-7b89f50203-item-9dc886373b'> klik hier voor meer informatie</a>" . "<br>" . "<img src='images/QuarterPounder.jpg' alt='Hier hoort een Quarter Pounder te staan'>";
                 }
                 else
                 {
-                    echo "De Mc Chicken past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten";
+                    echo "De Mc Chicken past het beste bij jou<br>" . "Je punten aantal was " . $allevragen . "punten" . "<br>" . "<a href='https://www.mcdonalds.com/nl/nl-nl/product/mcchicken.html#accordion-7b89f50203-item-9dc886373b'> klik hier voor meer informatie</a>" . "<br>" . "<img src='images/McChicken.jpg' alt='Hier hoort een Mc Chicken te staan'>";
                 }
                 ?>
 
